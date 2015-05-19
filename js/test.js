513393ec47d5160760b0418421645a7bc6b65af0
JuliuszKwiatkowski <kwiatkowski_juliusz@hotmail.com>
2015-05-19 13:20:54 +0100

    final
    final version

--- js/test.js
+++ js/test.js
@@ -3,7 +3,9 @@
 	
 	//this is the bit that's broken
 	$.each(data.items, function(i,val) {
-		$("#results").append($('<li class="grid-33 mobile-grid-100"><img width="100%" data-src='+ val.media.m +' src='+ val.media.m +'/></li>'));
+		var title;
+		if (val.title === ""){ title="No title"; }else{ title = val.title; }
+		$("#results").append($('<li class="grid-33 mobile-grid-100"><img width="100%" data-src='+ val.media.m +' src='+ val.media.m +'/><div class="grid-100 mobile-grid-100"><h1>'+title+'</h1><h2>Author: '+ val.author+'</h2></div></li>'));
 	});
 	};
 	
@@ -38,6 +40,7 @@
 			
 	}
 	$(function(){
+		
 		$(document).on('click', '#results li', function(){
 			var base = $(this),
 			baseImg = base.find('img');
@@ -54,8 +57,12 @@
 		       handleKeyPress('right');
 		    }
 		});
+		var container = document.querySelector('#results');
 		
 		$(window).bind("load", function() {
+			var msnry = new Masonry( container, {
+				itemSelector: 'li'
+			});
 			$('#results li').each(function() {
 				if($(this).find('img').data('src') == localStorage.getItem('lastSelectedItem')){
 					$(this).addClass('selected').find('img').addClass('selected');	
